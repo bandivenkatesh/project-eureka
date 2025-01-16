0c46cc851c60506543d57b49e68ad4685428a4d7
bandivenkatesh <bandivenky2222@gmail.com>
2025-01-16 13:19:01 +0530

    fixed pipeline code

--- src/main/java/com/lerner/i27eureka/I27EurekaApplication.java
+++ src/main/java/com/lerner/i27eureka/I27EurekaApplication.java
@@ -3,13 +3,12 @@ package com.lerner.i27eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
-@ComponentScan(basePackages = "com.lerner.i27eureka")
 public class I27EurekaApplication {
     public static void main(String[] args) {
+        System.setProperty("spring.cloud.bootstrap.enabled", "true");
         SpringApplication.run(I27EurekaApplication.class, args);
     }
 }
